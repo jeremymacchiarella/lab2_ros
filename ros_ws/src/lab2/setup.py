9666d03a993317f04f6cfa8acedf35b2162693ee
jeremymacchiarella <jeremy.macchiarella@gmail.com>
2025-09-30 23:44:59 +0000

    package name

--- ros_ws/src/lab2/setup.py
+++ ros_ws/src/lab2/setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        (os.path.join('share', lab2), glob('launch/*.py')),
+        (os.path.join('share', package_name), glob('launch/*.py')),
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
